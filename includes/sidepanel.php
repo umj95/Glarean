c204a872b2ee15e65415715d856787961bdeb4ef
Janosch Umbreit <janosch.umbreit@stud.uni-regensburg.de>
2021-07-05 11:41:06 +0200

    options select copied from live page

--- includes/sidepanel.php
+++ includes/sidepanel.php
@@ -11,7 +11,7 @@
     }
   }
 ?>
-<button name="options" class="panel" onclick="openPanel('optionsPanel')"></button>
+<button class="panel" onclick="openPanel('optionsPanel')"></button>
 <div id="optionsPanel" class="panel">
   <div id="closer">
     <a href="javascript:void(0)" class="closebtn" onclick="closePanel('optionsPanel')">&times;</a>
@@ -79,7 +79,7 @@
         </div>
       </fieldset>
       <label for="margins">Änderungen vornehmen</label>           <!-- Submit selection -->
-      <input type="submit" name="Änderungen vornehmen" value="submit"/>
+      <input type="submit" name="submit" value="submit"/>
     </form>
     <p><a href="<?php 
       $thisChapter = $chapterOptions['currentChapter'];
@@ -112,3 +112,4 @@
     echo $url;
   }
 ?>
+<!-- Commit comment -->
