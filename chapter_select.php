bd40aadf9bd311ec347b1ff907729dc69535aa61
Janosch Umbreit <janosch.umbreit@stud.uni-regensburg.de>
2021-07-16 10:16:27 +0200

    Changed heading sizes

--- chapter_select.php
+++ chapter_select.php
@@ -7,6 +7,15 @@
         <h2>Kapitel</h2>
         <p>Bitte wählen Sie hier Kapitel und Sprache aus. Sie können auch innerhalb eines Kapitels Passagen in der Übersetzung / im Original lesen.</p>
         <p>Bisher wurde das folgende Kapitel aufbereitet:</p>
+        <h3>Buch 1</h3>
+        <ul>
+
+        </ul>
+        <h3>Buch 2</h3>
+        <ul>
+
+        </ul>
+        <h3>Buch 3</h3>
         <ul>
           <li><em>De Symphonetarum Ingenio</em> · <span class="tooltip"><a href="chapter.php?currentChapter=3_Symph&mainLanguage=_lat">Latein</a><span class="tiptext">Dieses Kapitel auf latein Lesen</span></span> · <span class="tooltip"><a href="chapter.php?currentChapter=3_Symph&mainLanguage=_deu">Deutsch</a><span class="tiptext">Dieses Kapitel auf deutsch lesen</span></span></li>
         </ul>
