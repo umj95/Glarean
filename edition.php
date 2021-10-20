11bd8c3c9eecb9704e7f7cb37e3e2fbdc96a7a72
Janosch Umbreit <umj.ninetyfive@gmail.com>
2021-10-20 15:30:05 +0200

    fixed typo

--- edition.php
+++ edition.php
@@ -2,7 +2,7 @@
   $currentFile = basename(__FILE__);
   session_start();
   $pageLang = $_SESSION['lang'];
-  include("backend_functions");
+  include("backend_functions.php");
   include("static_texts.php");
   include("includes/header.php");
 ?>
