dd1a2da68489c8a77cf36ea7d9a27465b9a64f77
Janosch Umbreit <umj.ninetyfive@gmail.com>
2022-01-04 12:00:34 +0100

    tutorial debugging

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ session_start();
         </div>
         <script type="text/javascript">
           window.onhashchange = function () {
+            console.log("Tutorial!");
             //window.onload
             let hash = window.location.hash;
             if(hash == "#tutorial") {
